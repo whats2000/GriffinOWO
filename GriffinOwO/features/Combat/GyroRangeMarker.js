d13857964692ee8d4dbbee8855107d945cf6e259
whats2000 <60466660+whats2000@users.noreply.github.com>
2023-07-24 17:14:35 +0800

    Update GyroRangeMarker.js

--- GriffinOwO/features/Combat/GyroRangeMarker.js
+++ GriffinOwO/features/Combat/GyroRangeMarker.js
@@ -13,36 +13,45 @@ function getVec3Pos(vec) {
     return [vec.field_72450_a, vec.field_72448_b, vec.field_72449_c]
 }
 
+function getVec3iPos(vec) {
+    // [Vec3i.getX(), Vec3i.getY(), Vec3i.getZ()]
+    return [parseInt(vec.func_177958_n()), parseInt(vec.func_177956_o()), parseInt(vec.func_177952_p())]
+}
+
 registerEventListener(() => Settings.gyroRangeMarker,
     register("renderWorld", (partialTick) => {
         if (!Player.getHeldItem()?.getName()?.endsWith("Gyrokinetic Wand")) return;
 
-        // Get more correct by eye distance
-        const vec1 = Player.getPlayer().func_174824_e(partialTick); // getPositionEyes
-        const vec2 = Player.getPlayer().func_70676_i(partialTick);  // getLook
-        const [vecX, vecY, vecZ] = getVec3Pos(vec2);
-        const vec3 = vec1.func_72441_c(vecX * 20, vecY * 20, vecZ * 20);
-        const moveObject = World.getWorld().func_147447_a(vec1, vec3, true, false, true);
+        // Get block looking at
+        const moveObject = Player.getPlayer().func_174822_a(25, partialTick);
 
         // If on air return
         if (moveObject.field_72313_a.toString() !== "BLOCK") return; // MovingObjectPosition.typeOfHit
 
-        // If top of it not air return
-        const topBlock = World.getWorld().func_180495_p(moveObject.func_178782_a().func_177984_a()/* getBlockPos().up() */);
-        if (topBlock.toString() !== "minecraft:air") return;
+        // If top of it is slab, block, stair retun
+        const topBlockState = World.getWorld().func_180495_p(moveObject.func_178782_a().func_177984_a()); // mc.theWorld.getBlockState(moveObject.getBlockPos().up())
+        const topBlock = topBlockState.func_177230_c(); // topBlockState.getBlock()
+        if (topBlock instanceof Java.type("net.minecraft.block.BlockSlab")) return;
+
+        if (topBlock instanceof Java.type("net.minecraft.block.BlockStairs")) {
+            const halfValue = topBlockState.func_177229_b(topBlock.field_176308_b); // topBlockState.getValue(topBlock.Half)
+            if (halfValue.toString() === "bottom") return;
+        }
+
+        if (topBlock.func_149730_j()) return; //isFullBlock()
 
         // Get the position
-        const [x, y, z] = getVec3Pos(moveObject.field_72307_f); // MovingObjectPosition.hitVec
+        const [x, y, z] = getVec3iPos(moveObject.func_178782_a()); // MovingObjectPosition.blockPos
 
         // Check if in CD
         const [r, g, b] = gravityStormUsedTime < Date.now() ? [0, 1, 0] : [1, 0, 0];
 
-        RenderLib.drawCyl(x, y, z, 10, 10, 0.25, 30, 1, 0, 90, 90, r, g, b, 0.5, false, false);
+        RenderLib.drawCyl(x + 0.5, y + 1, z + 0.5, 10, 10, 0.25, 30, 1, 0, 90, 90, r, g, b, 0.5, false, false);
 
         const playerPos = [Player.getX(), Player.getY(), Player.getZ()];
         const distance = Math.floor(Math.sqrt(Math.pow(x - playerPos[0], 2) + Math.pow(y - playerPos[1], 2) + Math.pow(z - playerPos[2], 2)));
-        if (distance > 5)
-            renderBeaconBeam(x - 0.5, y, z - 0.5, r, g, b, 0.2, false);
+
+        renderBeaconBeam(x, y + 1, z, r, g, b, 0.2, false);
     })
 );
 
