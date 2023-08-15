10727be4678960531fd2c1bcf45221b9e06d5cb6
whats2000 <60466660+whats2000@users.noreply.github.com>
2023-08-15 23:39:23 +0800

    Add Plhlegblast in Lava Fishing Alert

--- GriffinOwO/features/CrimsonIsland/LavaFishingAlert.js
+++ GriffinOwO/features/CrimsonIsland/LavaFishingAlert.js
@@ -96,3 +96,50 @@ registerEventListener(() => Settings.lavaSeaCreature && checkInWorld("Crimson Is
         });
     })
 );
+
+registerEventListener(() => Settings.lavaSeaCreature && checkInWorld("Crimson Isle"),
+    register("Chat", (event) => {
+        const formatted_message = ChatLib.getChatMessage(event, true);
+
+        if (!formatted_message.includes("&r&aWOAH! A Plhlegblast appeared.&r")) return;
+
+        let channel = "";
+
+        switch (Settings.lavaSeaCreatureAlertChat) {
+            case 0:
+                channel = "pc";
+                break;
+            case 1:
+                channel = "gc";
+                break;
+            case 2:
+                channel = "ac";
+                break;
+            default:
+                channel = "pc";
+        }
+
+        const scoreboard = Scoreboard.getLines().map(a => { return ChatLib.removeFormatting(a) });
+
+        let location = "";
+
+        for (let line of scoreboard) {
+            if (line.includes("⏣")) {
+                location = line;
+                break;
+            }
+        }
+
+        const playerX = Math.floor(Player.getX());
+        const playerY = Math.floor(Player.getY());
+        const playerZ = Math.floor(Player.getZ());
+
+        registerCommand(() => {
+            ChatLib.command(`${channel} x: ${playerX}, y: ${playerY}, z: ${playerZ}`)
+        });
+
+        registerCommand(() => {
+            ChatLib.command(`${channel} [!] Plhlegblast is spawned at [${location} ] [!]`)
+        });
+    })
+);
